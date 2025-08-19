a400acbec11a464eb9da6f5aaac762df40889218
Si-gi <hidji@protonmail.com>
2025-08-19 11:44:49 +0200

    refacto tempTableQueryTest

--- tests/TempTableQueryTest.php
+++ tests/TempTableQueryTest.php
@@ -4,50 +4,93 @@ declare(strict_types=1);
 
 namespace Sigi\Tests\TempTableBundle;
 
+
+use Doctrine\DBAL\Result;
 use Doctrine\DBAL\Connection;
-use Iterator;
-use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
-use Prophecy\PhpUnit\ProphecyTrait;
+use Doctrine\DBAL\Query\QueryBuilder;
+use PHPUnit\Framework\MockObject\MockObject;
 use Sigi\TempTableBundle\Service\TempTableQuery;
 
 class TempTableQueryTest extends TestCase
 {
-    use ProphecyTrait;
+    private MockObject|Connection $connectionMock;
+    private MockObject|Result $resultMock;
+    private MockObject|QueryBuilder $queryBuilderMock;
+    private TempTableQuery $tempTableQuery;
 
-    public function testQuery(): void
+    protected function setUp(): void
     {
-        // $connection = $this->prophesize(Connection::class);
-        // $connection->fetchAllAssociative()->shouldBeCalled();
-        $connection = $this->createMock(Connection::class);
-        $connection->expects($this->once())
-        ->method("fetchAllAssociative")
-        ->willReturn([]);
-        $tempTableQuery = new TempTableQuery($connection);
-        $tempTableQuery->query("table");
-
+        $this->connectionMock = $this->createMock(Connection::class);
+        $this->resultMock = $this->createMock(Result::class);
+        $this->queryBuilderMock = $this->createMock(QueryBuilder::class);
+        $this->tempTableQuery = new TempTableQuery($this->connectionMock);
     }
 
-    #[DataProvider("buildQueryProvider")]
-    public function testBuildSelectQuery(string $table, array $conditions, string $expectedSql): void
+    public function testQueryWithoutConditions(): void
     {
-        $connection = $this->createMock(Connection::class);
-        $tempTableQuery = new TempTableQuery($connection);
-        $got = $tempTableQuery->buildSelectQuery("table", $conditions);
-        $this->assertEquals($expectedSql, $got[0]);
+        $expectedData = [['id' => 1, 'name' => 'test']];
+        
+        // Configuration du mock QueryBuilder
+        $this->queryBuilderMock
+            ->expects($this->once())
+            ->method('select')
+            ->with('*')
+            ->willReturnSelf();
+
+        $this->queryBuilderMock
+            ->expects($this->once())
+            ->method('executeQuery')
+            ->willReturn($this->resultMock);
+
+        $this->resultMock
+            ->expects($this->once())
+            ->method('fetchAllAssociative')
+            ->willReturn($expectedData);
+
+        $this->connectionMock
+            ->expects($this->once())
+            ->method('createQueryBuilder')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->connectionMock
+            ->expects($this->once())
+            ->method('quoteIdentifier')
+            ->with('test_table')
+            ->willReturn('"test_table"');
+
+        $this->queryBuilderMock
+            ->expects($this->once())
+            ->method('from')
+            ->with('"test_table"')
+            ->willReturnSelf();
+
+        $result = $this->tempTableQuery->query('test_table', [], 1, 1);
+        $this->assertEquals($expectedData, $result);
     }
 
-    public static function buildQueryProvider(): Iterator
+    public function testCreateQueryBuilderReturnsDoctrineQueryBuilder(): void
     {
-        yield [
-            'table', 
-            [],
-            'SELECT * FROM "table"'
-        ];
-        yield [
-            "table", 
-            ["name" => "john", "lastname" => "does"], 
-            'SELECT * FROM "table" WHERE "name" = :name AND "lastname" = :lastname'
-        ];
+        $this->connectionMock
+            ->expects($this->once())
+            ->method('createQueryBuilder')
+            ->willReturn($this->queryBuilderMock);
+
+        $this->connectionMock
+            ->expects($this->once())
+            ->method('quoteIdentifier')
+            ->with('test_table')
+            ->willReturn('"test_table"');
+
+        $this->queryBuilderMock
+            ->expects($this->once())
+            ->method('from')
+            ->with('"test_table"')
+            ->willReturnSelf();
+
+        $result = $this->tempTableQuery->createQueryBuilder('test_table');
+        
+        $this->assertSame($this->queryBuilderMock, $result);
     }
+
 }
